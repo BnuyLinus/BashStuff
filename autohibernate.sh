27015b2747a970f78b9e44c7261d8248f1336c16
BnuyLinus <155905054+BnuyLinus@users.noreply.github.com>
2024-03-12 15:31:18 +0100

    Updated autohibernate.sh - Comment typos
    Fixed comment typos.

--- autohibernate.sh
+++ autohibernate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# autohibernate.sh - This script checks if no user has been logged on for 0 minutees and then hibernate this intance.
+# autohibernate.sh - This script checks if no user has been logged on for 30 minutes and then hibernate this intance.
 # This script is intended for AWS EC2 instances running AWS Linux.
 # Crontab will run this cript every 30 minutes if set up correctly:
 # */30 * * * * /path/to/autohibernate.sh
